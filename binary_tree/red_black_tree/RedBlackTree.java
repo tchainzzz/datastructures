431e428f843a14e216e4d23e1bbba070b30cb64f
tchainzzz <tchang97@stanford.edu>
2018-10-20 14:18:45 -0700

    Created initial files and method stubs

---
+++ binary_tree/red_black_tree/RedBlackTree.java
@@ -0,0 +1,26 @@
+/** 
+ This class implements a self balancing red black tree.
+
+*/
+
+package binary_tree.red_black_tree;
+
+public class RedBlackTree {
+
+    private class TreeNode {
+
+    }
+
+    public void insert() {
+
+    }
+
+    public void delete() {
+
+    }
+
+    public void find() {
+        
+    }
+
+}
